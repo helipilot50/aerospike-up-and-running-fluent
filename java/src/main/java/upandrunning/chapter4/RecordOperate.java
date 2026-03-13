4e2dac628221da270bf3a5452dd020702124e4cc
Peter Milne <helipilot50@gmail.com>
2026-03-13 14:55:28 +0100

    chapter 4 outline

---
+++ java/src/main/java/upandrunning/chapter4/RecordOperate.java
@@ -0,0 +1,10 @@
+package upandrunning.chapter4;
+
+public class RecordOperate {
+    public static void main(String[] args) {
+        String host = "192.168.18.166";
+        int port = 43000;
+        System.out.println("Connecting to Aerospike at " + host + ":" + port);
+
+    }
+}
